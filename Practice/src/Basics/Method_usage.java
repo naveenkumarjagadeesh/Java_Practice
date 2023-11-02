c9ae99727b49d62f8b51d3fb392ba41a94ada258
Naveen <naveenkumar.jagadeesh@gmail.com>
2023-11-02 16:55:58 +0530

    adding one extra line to create a pr from the branch1 to orgin/master

---
+++ Practice/src/Basics/Method_usage.java
@@ -0,0 +1,17 @@
+package Basics;
+
+
+public class Method_usage {
+	
+	static void printing(String s)
+	{
+		System.out.println(s); 	
+	}
+
+	public static void main(String[] args) {
+		
+		String s = "NAveen is the creater of this program"; 
+		
+		printing(s);
+	}
+}
