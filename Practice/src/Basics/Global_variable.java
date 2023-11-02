961f142acd9b9e49456ed5ab3e556c2da62cb02d
Naveen <naveenkumar.jagadeesh@gmail.com>
2023-11-02 16:17:53 +0530

    "global and local variable programs"

---
+++ Practice/src/Basics/Global_variable.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package Basics;
+
+/**
+ * @author Naveen
+ *
+ */
+public class Global_variable {
+
+	String st = "Naveen";
+	int i = 55;
+	double db = 75.056778;
+	float flt = 15.933655738f;
+	static boolean bl = true;
+		
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Global_variable gv = new Global_variable();
+		System.out.println(gv.st);
+		System.out.println(gv.i);
+		System.out.println(gv.db);
+		System.out.println(gv.flt);
+		System.out.println(bl);
+		
+
+	}
+
+}
---
+++ Practice/src/Basics/local_variables.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package Basics;
+
+/**
+ * @author Naveen
+ *
+ */
+public class local_variables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String st = "Naveen";
+		int i = 55;
+		boolean bl = false;
+		double dbl = 55.0550;
+		float flt = 25.0345f;
+		System.out.println(st);
+		System.out.println(i);
+		System.out.println(bl);
+		System.out.println(dbl);
+		System.out.println(flt);
+		
+		
+		
+		
+
+	}
+
+}
